c123d5f397e8103ff0cf50327d4e628aac8f7992
MASUTANI Yasuhiro <masutani@isc.osakac.ac.jp>
2017-11-27 20:30:15 +0900

    最初のコミット

---
+++ README.md
@@ -0,0 +1,79 @@
+# RTC:PCL Grabber for Microsoft Kinect v2
+
+大阪電気通信大学  
+升谷 保博  
+2017年11月27日
+
+## はじめに
+
+- Microsoft Kinect v2から深度と色の情報を読み取り，RTC:PCLのPointCloud型（`PointCloudTypes::PointCoud`）を
+出力するRTコンポーネントです．
+- 以下の環境で開発，動作確認しています．
+  - Windows 10 64bit版
+  - Visual Studio 2012
+  - OpenRTM-aist 1.1.2 64bit版
+  - Point Cloud Library 1.7.2 AllinOne (VS2012 64bit用)
+  - Microsoft Kinect Xbox One (Kinect v2)
+  - Kinect for Windows SDK v2.0
+- 杉浦 司 (UnaNancyOwen) 氏の
+[Grabber of Point Cloud Library based on Kinect for Windows SDK ](https://github.com/UnaNancyOwen/KinectGrabber/tree/Kinect2Grabber)のコードを
+そのまま利用しています（`KinectGrabber\kinect2_grabber.h`）．
+- CMakeでKinect SDKを見つけるモジュールも杉浦 司氏の
+[`FindKinectSDK2.cmake`](https://github.com/UnaNancyOwen/KinectGrabber/blob/Kinect2Grabber/Sample/FindKinectSDK2.cmake)
+をそのまま使っています．
+- `pointcloud.idl` は，Geoffrey Biggs (gbiggs)氏の
+[RT-Components for the Point Cloud Library](https://github.com/gbiggs/rtcpcl/)
+に[含まれているもの](https://github.com/gbiggs/rtcpcl/blob/master/pc_type/pointcloud.idl)
+をそのまま使っています．
+
+## 仕様
+
+- height: 424
+- width: 512
+- type: "xyzrgb"
+- is_bigendian: 0
+- point_step: 16
+- row_step: 8192
+- is_dense: 0
+- fields (name,offset,data_type,count)
+  - 0: "x",0,6,4
+  - 1: "y",4,6,4
+  - 2: "z",8,6,4
+  - 3: "b",12,1,1
+  - 4: "g",13,1,1
+  - 5: "r",14,1,1
+
+## インストール
+
+- [OpenRTM-aist 1.1.2](http://www.openrtm.org/openrtm/ja/node/6034)をインストール．
+- [PCL-1.7.2のWindows用AllInOne](https://onedrive.live.com/?authkey=!ACFnPNzPhXJ0FtU&id=EC9EBB2646FF189A!49089&cid=EC9EBB2646FF189A)
+をインストール（2017年3月下旬に削除されたようです）．
+- [Kinect for Windows SDK v2.0](https://www.microsoft.com/en-us/download/details.aspx?id=44561)
+をインストール．
+- [KinectToPC](https://github.com/MasutaniLab/Kinect2ToPC)
+をクローンかダウンロードする．
+- CMake
+  - ビルドディレクトリはトップ直下の`build`
+  - ConfigureはVisual Studio 64bit
+- `build\Kinect2ToPC.sln`をVisual Studioで開く．
+- パフォーマンスを出すために，Releaseでビルドがお勧め．
+
+## 使い方
+
+- KinectをUSBポートに接続する．
+- 出力されるデータ量が多いので，CORBAのデフォルトの設定ではエラーになります．
+rtc.confに`corba.args: -ORBgiopMaxMsgSize`の設定が必要です．
+トップディレクトリのrtc.confでは`corba.args: -ORBgiopMaxMsgSize 20971520`
+にしています（デフォルト値の10倍）．
+- コンポーネントを起動するバッチファイル`Kinect2ToPC.bat`を用意しています．
+  - ビルドディレクトリがトップ直下の`build`であることを仮定しています．
+  - 環境変数`RTM_VC_CONFIG`を`Debug`か`Release`に設定してください．
+- 動作確認のための接続相手として，
+[PointCloudViewer](https://github.com/MasutaniLab/PointCloudViewer)
+を使ってください．
+
+## 既知の問題・TODO
+
+- [`pointcloud.idl`](https://github.com/gbiggs/rtcpcl/blob/master/pc_type/pointcloud.idl)に記述されている仕様と，[RT-Components for the Point Cloud Library](https://github.com/gbiggs/rtcpcl/)の関数の仕様が異なっていますが，`pointcloud.idl`本来の仕様のデータを出力しています．
+- 1回に送出するデータ量が約3.3MB．もっと圧縮して送る方がいいのかもしれません．
+- Windowsでしか動作しません．
---
+++ src/Kinect2ToPC.cpp
@@ -0,0 +1,246 @@
+// -*- C++ -*-
+/*!
+ * @file  Kinect2ToPC.cpp
+ * @brief RTC:PCL Grabber for Kinect
+ * @date $Date$
+ *
+ * $Id$
+ */
+
+#include "kinect2_grabber.h"
+#include "Kinect2ToPC.h"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* kinect2topc_spec[] =
+  {
+    "implementation_id", "Kinect2ToPC",
+    "type_name",         "Kinect2ToPC",
+    "description",       "RTC:PCL Grabber for Kinect",
+    "version",           "1.0.0",
+    "vendor",            "MasutaniLab",
+    "category",          "RTCPCL",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+Kinect2ToPC::Kinect2ToPC(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_pcOut("pc", m_pc)
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+Kinect2ToPC::~Kinect2ToPC()
+{
+}
+
+
+
+RTC::ReturnCode_t Kinect2ToPC::onInitialize()
+{
+  cout << "Kinect2ToPC::onInitialize()" << endl;
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  
+  // Set OutPort buffer
+  addOutPort("pc", m_pcOut);
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // </rtc-template>
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t Kinect2ToPC::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t Kinect2ToPC::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t Kinect2ToPC::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+RTC::ReturnCode_t Kinect2ToPC::onActivated(RTC::UniqueId ec_id)
+{
+  cout << "Kinect2ToPC::onActivated()" << endl;
+  try {
+    m_interface = boost::make_shared<pcl::Kinect2Grabber>();
+    m_pc.type = "xyzrgb";
+    m_pc.fields.length(6);
+    m_pc.fields[0].name = "x";
+    m_pc.fields[0].offset = 0;
+    m_pc.fields[0].data_type = PointCloudTypes::FLOAT32;
+    m_pc.fields[0].count = 4;
+    m_pc.fields[1].name = "y";
+    m_pc.fields[1].offset = 4;
+    m_pc.fields[1].data_type = PointCloudTypes::FLOAT32;
+    m_pc.fields[1].count = 4;
+    m_pc.fields[2].name = "z";
+    m_pc.fields[2].offset = 8;
+    m_pc.fields[2].data_type = PointCloudTypes::FLOAT32;
+    m_pc.fields[2].count = 4;
+    m_pc.fields[3].name = "b";
+    m_pc.fields[3].offset = 12;
+    m_pc.fields[3].data_type = PointCloudTypes::UINT8;
+    m_pc.fields[3].count = 1;
+    m_pc.fields[4].name = "g";
+    m_pc.fields[4].offset = 13;
+    m_pc.fields[4].data_type = PointCloudTypes::UINT8;
+    m_pc.fields[4].count = 1;
+    m_pc.fields[5].name = "r";
+    m_pc.fields[5].offset = 14;
+    m_pc.fields[5].data_type = PointCloudTypes::UINT8;
+    m_pc.fields[5].count = 1;
+    m_pc.is_bigendian = false;
+    m_pc.point_step = 16;
+    m_pc.is_dense = false;
+    boost::function<void (const pcl::PointCloud<PointT>::ConstPtr&)> f = boost::bind(&Kinect2ToPC::cloud_cb, this, _1);
+    m_interface->registerCallback(f);
+    m_interface->start();
+  } catch (pcl::io::IOException& e) {
+    cerr << "Failed to create a grabber: " << e.what () << endl;
+    return RTC::RTC_ERROR;
+  } catch (...) {
+    cerr << "An exception occurred while starting grabber" << endl;
+    return RTC::RTC_ERROR;
+  }
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t Kinect2ToPC::onDeactivated(RTC::UniqueId ec_id)
+{
+  cout << "Kinect2ToPC::onDeactivated()" << endl;
+  m_interface->stop();
+  m_interface = nullptr;
+
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t Kinect2ToPC::onExecute(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t Kinect2ToPC::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t Kinect2ToPC::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t Kinect2ToPC::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t Kinect2ToPC::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t Kinect2ToPC::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+void Kinect2ToPC::cloud_cb(const pcl::PointCloud<PointT>::ConstPtr &cloud)
+{
+  setTimestamp(m_pc);
+  m_pc.width = cloud->width;
+  m_pc.height = cloud->height;
+  m_pc.row_step = m_pc.point_step*m_pc.width;
+  m_pc.data.length(m_pc.height*m_pc.row_step);
+
+  float *dst_cloud = (float *)m_pc.data.get_buffer();
+  for (unsigned int i=0; i<cloud->points.size(); i++){
+    dst_cloud[0] = -cloud->points[i].x;
+    dst_cloud[1] =  cloud->points[i].y;
+    dst_cloud[2] = -cloud->points[i].z;
+    dst_cloud[3] =  cloud->points[i].rgb;
+    dst_cloud += 4;
+  }
+  m_pcOut.write();
+
+  static int count = 0;
+  static double last = 0;
+
+  if (++count == 30)
+  {
+    double now = pcl::getTime();
+    std::cout << "Average framerate: " << double(count)/double(now - last) << " Hz" <<  std::endl;
+    count = 0;
+    last = now;
+  }
+}
+
+
+extern "C"
+{
+ 
+  void Kinect2ToPCInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(kinect2topc_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<Kinect2ToPC>,
+                             RTC::Delete<Kinect2ToPC>);
+  }
+  
+};
+
+
