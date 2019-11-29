6df8f285b501cb0da43da31e65a6f55527f5288b
森下 英治 <ht16a086@oecu.jp>
2019-11-29 14:19:32 +0900

    できる限りdoxygenのエラーを消した

--- src/Kinect2ToPC.cpp
+++ src/Kinect2ToPC.cpp
@@ -3,21 +3,19 @@
  * @file  Kinect2ToPC.cpp
  * @brief RTC:PCL Grabber for Kinect
  * @date $Date$
- *
+ * @Copyright 2019
  * $Id$
  */
 
 #include "kinect2_grabber.h"
 #include "Kinect2ToPC.h"
-
 #include <iostream>
 #include <string>
 using namespace std;
 
 // Module specification
 // <rtc-template block="module_spec">
-static const char* kinect2topc_spec[] =
-{
+static const char* kinect2topc_spec[] = {
   "implementation_id", "Kinect2ToPC",
   "type_name",         "Kinect2ToPC",
   "description",       "RTC:PCL Grabber for Kinect",
@@ -36,7 +34,6 @@ static const char* kinect2topc_spec[] =
   "conf.default.R_trimming_Y", "10",
   "conf.default.trimming_Z", "10",
   "conf.default.R_trimming_Z", "10",
-
   // Widget
   "conf.__widget__.trimming_X", "text",
   "conf.__widget__.R_trimming_X", "text",
@@ -45,14 +42,12 @@ static const char* kinect2topc_spec[] =
   "conf.__widget__.trimming_Z", "text",
   "conf.__widget__.R_trimming_Z", "text",
   // Constraints
-
   "conf.__type__.trimming_X", "int",
   "conf.__type__.R_trimming_X", "int",
   "conf.__type__.trimming_Y", "int",
   "conf.__type__.R_trimming_Y", "int",
   "conf.__type__.trimming_Z", "int",
   "conf.__type__.R_trimming_Z", "int",
-
   ""
 };
 // </rtc-template>
@@ -65,7 +60,6 @@ Kinect2ToPC::Kinect2ToPC(RTC::Manager* manager)
 // <rtc-template block="initializer">
 	: RTC::DataFlowComponentBase(manager),
 	m_pcOut("pc", m_pc)
-
 	// </rtc-template>
 {
 }
@@ -73,66 +67,51 @@ Kinect2ToPC::Kinect2ToPC(RTC::Manager* manager)
 /*!
 * @brief destructor
 */
-Kinect2ToPC::~Kinect2ToPC()
-{
+Kinect2ToPC::~Kinect2ToPC() {
 }
 
-
-
-RTC::ReturnCode_t Kinect2ToPC::onInitialize()
-{
+RTC::ReturnCode_t Kinect2ToPC::onInitialize() {
 	// Registration: InPort/OutPort/Service
 	// <rtc-template block="registration">
 	// Set InPort buffers
-
 	// Set OutPort buffer
 	addOutPort("pc", m_pcOut);
-
 	// Set service provider to Ports
-
 	// Set service consumers to Ports
-
 	// Set CORBA Service Ports
-
 	// </rtc-template>
-
 	// <rtc-template block="bind_config">
 	// Bind variables and configuration variable
-	bindParameter("trimming_X", m_trimming_X, "10");// プラス 単位m 送るデータが大きいためトリミングをする
-	bindParameter("R_trimming_X", m_R_trimming_X, "10");// マイナス
-	bindParameter("trimming_Y", m_trimming_Y, "10");// プラス
-	bindParameter("R_trimming_Y", m_R_trimming_Y, "10");// マイナス
-	bindParameter("trimming_Z", m_trimming_Z, "10");// プラス
-	bindParameter("R_trimming_Z", m_R_trimming_Z, "10");// マイナス
+	bindParameter("trimming_X", m_trimming_X, "10");  // プラス 単位m
+	bindParameter("R_trimming_X", m_R_trimming_X, "10");  // マイナス
+	bindParameter("trimming_Y", m_trimming_Y, "10");  // プラス
+	bindParameter("R_trimming_Y", m_R_trimming_Y, "10");  // マイナス
+	bindParameter("trimming_Z", m_trimming_Z, "10");  // プラス
+	bindParameter("R_trimming_Z", m_R_trimming_Z, "10");  // マイナス
+	//送るデータが大きいためトリミングをする
 	// </rtc-template>
-
 	return RTC::RTC_OK;
 }
 
 /*
-RTC::ReturnCode_t Kinect2ToPC::onFinalize()
-{
+RTC::ReturnCode_t Kinect2ToPC::onFinalize() {
   return RTC::RTC_OK;
 }
 */
 
 /*
-RTC::ReturnCode_t Kinect2ToPC::onStartup(RTC::UniqueId ec_id)
-{
+RTC::ReturnCode_t Kinect2ToPC::onStartup(RTC::UniqueId ec_id) {
   return RTC::RTC_OK;
 }
 */
 
 /*
-RTC::ReturnCode_t Kinect2ToPC::onShutdown(RTC::UniqueId ec_id)
-{
+RTC::ReturnCode_t Kinect2ToPC::onShutdown(RTC::UniqueId ec_id) {
   return RTC::RTC_OK;
 }
 */
 
-
-RTC::ReturnCode_t Kinect2ToPC::onActivated(RTC::UniqueId ec_id)
-{
+RTC::ReturnCode_t Kinect2ToPC::onActivated(RTC::UniqueId ec_id) {
 	RTC_INFO(("onActivated()"));
 	try {
 		m_interface = boost::make_shared<pcl::Kinect2Grabber>();
@@ -165,7 +144,8 @@ RTC::ReturnCode_t Kinect2ToPC::onActivated(RTC::UniqueId ec_id)
 		m_pc.is_bigendian = false;
 		m_pc.point_step = 16;
 		m_pc.is_dense = false;
-		boost::function<void(const pcl::PointCloud<PointT>::ConstPtr&)> f = boost::bind(&Kinect2ToPC::cloud_cb, this, _1);
+		boost::function<void(const pcl::PointCloud<PointT>::ConstPtr&)>
+		f = boost::bind(&Kinect2ToPC::cloud_cb, this, _1);
 		m_interface->registerCallback(f);
 		m_interface->start();
 	}
@@ -180,59 +160,48 @@ RTC::ReturnCode_t Kinect2ToPC::onActivated(RTC::UniqueId ec_id)
 	return RTC::RTC_OK;
 }
 
-
-RTC::ReturnCode_t Kinect2ToPC::onDeactivated(RTC::UniqueId ec_id)
-{
+RTC::ReturnCode_t Kinect2ToPC::onDeactivated(RTC::UniqueId ec_id) {
 	RTC_INFO(("onDeactivated()"));
 	m_interface->stop();
 	m_interface = nullptr;
-
 	return RTC::RTC_OK;
 }
 
-
-RTC::ReturnCode_t Kinect2ToPC::onExecute(RTC::UniqueId ec_id)
-{
+RTC::ReturnCode_t Kinect2ToPC::onExecute(RTC::UniqueId ec_id) {
 	return RTC::RTC_OK;
 }
 
 /*
-RTC::ReturnCode_t Kinect2ToPC::onAborting(RTC::UniqueId ec_id)
-{
+RTC::ReturnCode_t Kinect2ToPC::onAborting(RTC::UniqueId ec_id) {
   return RTC::RTC_OK;
 }
 */
 
 /*
-RTC::ReturnCode_t Kinect2ToPC::onError(RTC::UniqueId ec_id)
-{
+RTC::ReturnCode_t Kinect2ToPC::onError(RTC::UniqueId ec_id) {
   return RTC::RTC_OK;
 }
 */
 
 /*
-RTC::ReturnCode_t Kinect2ToPC::onReset(RTC::UniqueId ec_id)
-{
+RTC::ReturnCode_t Kinect2ToPC::onReset(RTC::UniqueId ec_id) {
   return RTC::RTC_OK;
 }
 */
 
 /*
-RTC::ReturnCode_t Kinect2ToPC::onStateUpdate(RTC::UniqueId ec_id)
-{
+RTC::ReturnCode_t Kinect2ToPC::onStateUpdate(RTC::UniqueId ec_id) {
   return RTC::RTC_OK;
 }
 */
 
 /*
-RTC::ReturnCode_t Kinect2ToPC::onRateChanged(RTC::UniqueId ec_id)
-{
+RTC::ReturnCode_t Kinect2ToPC::onRateChanged(RTC::UniqueId ec_id) {
   return RTC::RTC_OK;
 }
 */
 
-void Kinect2ToPC::cloud_cb(const pcl::PointCloud<PointT>::ConstPtr &cloud)
-{
+void Kinect2ToPC::cloud_cb(const pcl::PointCloud<PointT>::ConstPtr &cloud) {
 	setTimestamp(m_pc);
 	m_pc.width = cloud->width;
 	m_pc.height = cloud->height;
@@ -241,10 +210,12 @@ void Kinect2ToPC::cloud_cb(const pcl::PointCloud<PointT>::ConstPtr &cloud)
 
 	float *dst_cloud = (float *)m_pc.data.get_buffer();
 	for (unsigned int i = 0; i < cloud->points.size(); i++) {
-		if (m_trimming_X > -cloud->points[i].x && -cloud->points[i].x > -m_R_trimming_X &&
-			m_trimming_Y > -cloud->points[i].y && -cloud->points[i].y > -m_R_trimming_Y &&
-			m_trimming_Z > -cloud->points[i].z && -cloud->points[i].z > -m_R_trimming_Z)
-		{
+		if (m_trimming_X > -cloud->points[i].x &&
+			-cloud->points[i].x > -m_R_trimming_X &&
+			m_trimming_Y > -cloud->points[i].y &&
+			-cloud->points[i].y > -m_R_trimming_Y &&
+			m_trimming_Z > -cloud->points[i].z &&
+			-cloud->points[i].z > -m_R_trimming_Z) {
 			dst_cloud[0] = -cloud->points[i].x;
 			dst_cloud[1] = cloud->points[i].y;
 			dst_cloud[2] = -cloud->points[i].z;
@@ -257,8 +228,7 @@ void Kinect2ToPC::cloud_cb(const pcl::PointCloud<PointT>::ConstPtr &cloud)
 	static int count = 0;
 	static double last = 0;
 
-	if (++count == 30)
-	{
+	if (++count == 30) {
 		double now = pcl::getTime();
 		RTC_INFO(("Average framerate: %lf Hz", double(count) / double(now - last)));
 		count = 0;
@@ -266,18 +236,11 @@ void Kinect2ToPC::cloud_cb(const pcl::PointCloud<PointT>::ConstPtr &cloud)
 	}
 }
 
-
-extern "C"
-{
-
-	void Kinect2ToPCInit(RTC::Manager* manager)
-	{
+extern "C" {
+	void Kinect2ToPCInit(RTC::Manager* manager) {
 		coil::Properties profile(kinect2topc_spec);
 		manager->registerFactory(profile,
 			RTC::Create<Kinect2ToPC>,
 			RTC::Delete<Kinect2ToPC>);
 	}
-
 };
-
-
